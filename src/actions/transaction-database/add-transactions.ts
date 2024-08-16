6985dd3688f30fb50b6313bb645780e6fd7b16fe
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-08-16 14:34:43 -0700

    Update and format transactions actions to new database

--- src/actions/transaction-database/add-transactions.ts
+++ src/actions/transaction-database/add-transactions.ts
@@ -1,19 +1,51 @@
-import prisma from '@/lib/db';
 import { getAccounts } from '../quickbooks/get-accounts';
+import { db } from '@/db/index';
+import {
+  Transaction as DrizzleTransaction,
+  TransactionsToClassifications,
+  Classification,
+} from '@/db/schema';
 import type { Account } from '@/types/Account';
 import type { Transaction } from '@/types/Transaction';
+import { eq } from 'drizzle-orm';
 
 export async function addTransactions(
   transactions: Transaction[]
 ): Promise<void> {
   for (const transaction of transactions) {
     try {
-      // Check if a TransactionClassification with the same name already exists in the database.
-      const existingTransaction =
-        await prisma.transactionClassification.findUnique({
-          where: { transactionName: transaction.name },
-          include: { classifications: true },
-        });
+      // Check for an existing transaction with the same name.
+      const existingTransaction = await db
+        .select()
+        .from(DrizzleTransaction)
+        .where(eq(DrizzleTransaction.transactionName, transaction.name));
+
+      // Get the transaction to classificaion relations for the transaction.
+      const transactionClassifications = await db
+        .select()
+        .from(TransactionsToClassifications)
+        .where(
+          eq(
+            TransactionsToClassifications.transactionId,
+            existingTransaction[0].id
+          )
+        );
+
+      // Create an array to store the classifications for the transaction.
+      const classifications: {
+        id: number;
+        category: string;
+        count: number;
+      }[] = [];
+
+      // Get the classification for each relationship and add it to the classifications array.
+      for (const relationship of transactionClassifications) {
+        const classification = await db
+          .select()
+          .from(Classification)
+          .where(eq(Classification.id, relationship.classificationId));
+        classifications.push(classification[0]);
+      }
 
       // Get the list of user accounts and parse them to a list of Account objects.
       const accounts = await getAccounts();
@@ -37,43 +69,63 @@ export async function addTransactions(
       const baseTransactionCategory =
         accountNamesToCategories[deepestTransactionAccount];
 
-      if (existingTransaction) {
-        // Find if the transaction has already been categorized and saved in the classifications array.
-        const existingCategory = existingTransaction.classifications.find(
-          (classification: { category: string }) =>
-            classification.category === baseTransactionCategory
-        );
+      // Check through array of classifications to see if the transaction has already been categorized.
+      const existingCategory = classifications.find(
+        (classification) => classification.category === baseTransactionCategory
+      );
 
+      // Create variables to store the IDs needed to update the relationship table.
+      let relatedTransactionId: number;
+      let relatedClassificationId: number;
+
+      // Create or update the classification for the transaction.
+      if (existingCategory) {
         // If the category already exists, increment its count by 1
-        if (existingCategory) {
-          await prisma.classification.update({
-            where: { id: existingCategory.id },
-            data: { count: { increment: 1 } },
-          });
-        } else {
-          // If the category doesn't exist, create a new classification with a count of 1.
-          await prisma.classification.create({
-            data: {
-              category: baseTransactionCategory,
-              count: 1,
-              transactionClassificationId: existingTransaction.id,
-            },
-          });
-        }
+        await db
+          .update(Classification)
+          .set({
+            count: existingCategory.count + 1,
+          })
+          .where(eq(Classification.id, existingCategory.id));
+
+        // Record the id of the updated classification.
+        relatedClassificationId = existingCategory.id;
       } else {
-        // If the TransactionClassification doesn't exist, create a new one with a count of 1.
-        await prisma.transactionClassification.create({
-          data: {
-            transactionName: transaction.name,
-            classifications: {
-              create: {
-                category: baseTransactionCategory,
-                count: 1,
-              },
-            },
-          },
-        });
+        // If the category doesn't exist, create a new classification with a count of 1.
+        const newClassification = await db
+          .insert(Classification)
+          .values({
+            category: baseTransactionCategory,
+            count: 1,
+          })
+          .returning();
+
+        // Record the id of the new classification.
+        relatedClassificationId = newClassification[0].id;
       }
+
+      // Create a new transaction if needed and record its id, or get the id of the existing transaction.
+      if (!existingTransaction[0]) {
+        // Create a new transaction with the transaction name.
+        const newTransaction = await db
+          .insert(DrizzleTransaction)
+          .values({
+            transactionName: transaction.name,
+          })
+          .returning();
+
+        // Record the id of the new transaction.
+        relatedTransactionId = newTransaction[0].id;
+      } else {
+        // Record the id of the existing transaction.
+        relatedTransactionId = existingTransaction[0].id;
+      }
+
+      // Create a new relationship between the transaction and classification.
+      await db.insert(TransactionsToClassifications).values({
+        transactionId: relatedTransactionId,
+        classificationId: relatedClassificationId,
+      });
     } catch (error) {
       // Catch and log any errors that occur during the transaction addition process.
       console.error('Error adding transaction:', error);
