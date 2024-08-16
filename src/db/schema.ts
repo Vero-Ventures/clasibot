c6404e3f903968ec8b00d233ae25b7480932a0bd
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-08-16 10:53:14 -0700

    First migration implemented.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,38 +1,56 @@
 // schema.ts
 
-import {
-  pgTable,
-  text,
-  integer,
-  serial,
-  primaryKey,
-} from 'drizzle-orm/pg-core';
+import { pgTable, text, integer, serial, primaryKey } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
 import { createId } from '@paralleldrive/cuid2';
 
 export const User = pgTable('User', {
   id: text('id').primaryKey().default(createId()),
-  first_name: text('first_name'),
-  last_name: text('last_name'),
+  firstName: text('first_name'),
+  lastName: text('last_name'),
   email: text('email').unique(),
   industry: text('industry'),
-  subscriptionId: text('subscriptionId')
+  subscriptionId: text('subscription_id')
     .unique()
     .references(() => Subscription.id, { onDelete: 'cascade' }),
 });
 
 export const Subscription = pgTable('Subscription', {
   id: text('id').primaryKey().default(createId()),
-  userId: text('userId').unique(),
-  stripeId: text('stripeId').unique(),
+  userId: text('user_id').unique(),
+  stripeId: text('stripe_id').unique(),
 });
 
-export const TransactionClassification = pgTable('TransactionClassification', {
+export const Transaction = pgTable('Transaction', {
   id: serial('id').primaryKey(),
-  transactionName: text('transactionName').unique(),
+  transactionName: text('transaction_name').unique(),
 });
+
+export const TransactionToClassificationsRelationship = relations(Transaction, ({ many }) => ({
+  classifications: many(Classification),
+}));
 
 export const Classification = pgTable('Classification', {
   id: serial('id').primaryKey(),
   category: text('category').unique(),
   count: integer('count'),
 });
+
+const ClassificationToTransactionsRelationship = relations(Classification, ({ many }) => ({
+  transactions: many(Transaction),
+}));
+
+export const TransactionsToClassifications = pgTable(
+  'TransactionsToClassifications',
+  {
+    transactionId: integer('transaction_id')
+      .notNull()
+      .references(() => Transaction.id),
+    classificationId: integer('classification_id')
+      .notNull()
+      .references(() => Classification.id),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.transactionId, t.classificationId] }),
+  }),
+);
