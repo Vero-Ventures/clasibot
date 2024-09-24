630342c7fa275c82f987fb92d795da897971cc1d
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-09-24 14:07:21 -0700

    Comment out update industry action file due to its removal in current version due to schema updates.

--- src/actions/update-industry.ts
+++ src/actions/update-industry.ts
@@ -1,23 +1,23 @@
-'use server';
-import { db } from '@/db/index';
-import { User } from '@/db/schema';
-import { eq } from 'drizzle-orm';
+// 'use server';
+// import { db } from '@/db/index';
+// import { User } from '@/db/schema';
+// import { eq } from 'drizzle-orm';
 
-export async function updateIndustry(industry: string, email: string) {
-  try {
-    // Update the user's industry using the email.
-    await db
-      .update(User)
-      .set({
-        industry: industry,
-      })
-      .where(eq(User.email, email));
+// export async function updateIndustry(industry: string, email: string) {
+//   try {
+//     // Update the user's industry using the email.
+//     await db
+//       .update(User)
+//       .set({
+//         industry: industry,
+//       })
+//       .where(eq(User.email, email));
 
-    // If the industry is updated successfully, return a success message.
-    return 'Industry Updated Successfully';
-  } catch (error) {
-    // Log error to console and return an error response string.
-    console.error(error);
-    return 'Error';
-  }
-}
+//     // If the industry is updated successfully, return a success message.
+//     return 'Industry Updated Successfully';
+//   } catch (error) {
+//     // Log error to console and return an error response string.
+//     console.error(error);
+//     return 'Error';
+//   }
+// }
