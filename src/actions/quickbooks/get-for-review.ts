9e5961caa747303b3a8ca02d3899fb92c9471255
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-09-26 11:25:49 -0700

    Update formatting to return a query response result.

--- src/actions/quickbooks/get-for-review.ts
+++ src/actions/quickbooks/get-for-review.ts
@@ -1,8 +1,9 @@
 'use server';
 
 import type { FormattedForReviewTransaction } from '@/types/ForReviewTransaction';
+import type { QueryResult } from '@/types/QueryResult';
 
-export async function getForReview(accountId: string) {
+export async function getForReview(accountId: string): Promise<QueryResult> {
   try {
     // Define the parameters for the call and the full endpoint to use.
     // This will need to be run for each bank account / credit card assosiated with a user.
@@ -19,38 +20,43 @@ export async function getForReview(accountId: string) {
     const response = await fetch(endpoint, {
       method: 'GET',
       headers: {
-        cookie:
-          `qbo.tkt=V1-11-B0atmppjt4lf5fnt15laho; qbo.agentid=9411821713535995; qbo.parentid=9341452698223021; qbo.authid=9341453042273832; SameSite=None`,
+        cookie: `qbo.tkt=V1-11-B0atmppjt4lf5fnt15laho; qbo.agentid=9411821713535995; qbo.parentid=9341452698223021; qbo.authid=9341453042273832; SameSite=None`,
       },
     });
 
     // If no valid response is given, get the response text and return it in a result object with an error result.
     if (!response.ok) {
       const errorText = await response.text();
-      return Response.json({
-        Result: 'Error',
-        Message:
+      return {
+        result: 'Error',
+        message:
           'Call made to Query API endpoint did not return a valid response.',
-        Detail: errorText,
-      });
+        detail: JSON.stringify(errorText),
+      };
     }
 
     // Get the response data, format it, and return it to the caller in a result object with a success result.
     const responseData = await response.json();
     const formattedResponse = readForReviewTransaction(responseData);
-    return Response.json({
-      Result: 'success',
-      Message:
+    return {
+      result: 'success',
+      message:
         'Request made to Query API endpoint was returned a valid response',
-      Detail: formattedResponse,
-    });
+      detail: formattedResponse,
+    };
   } catch (error) {
+    // Define a default error detail.
+    let errorDetail = 'An unexpected error occured.'
+    // Check if error is of type Error and update the detail if it is.
+    if (error instanceof Error) {
+        errorDetail = error.message
+    }
     // If there is an error calling the API, get the response error and return it in a result object with an error result.
-    return Response.json({
-      Result: 'Error',
-      Message: 'Call made to Query API endpoint resulted in error.',
-      Detail: error,
-    });
+    return {
+      result: 'Error',
+      message: 'Call made to Query API endpoint resulted in error.',
+      detail: errorDetail,
+    };
   }
 }
 
@@ -65,9 +71,10 @@ function readForReviewTransaction(responseData: {
       amount: number;
     },
   ];
-}): FormattedForReviewTransaction[] {
+}): string {
   const transactions = [];
   for (const transactionItem of responseData.items) {
+    console.log(transactionItem);
     // Only record expense (spending) transactions.
     if (transactionItem.amount < 0) {
       const newTransaction: FormattedForReviewTransaction = {
@@ -80,5 +87,5 @@ function readForReviewTransaction(responseData: {
       transactions.push(newTransaction);
     }
   }
-  return transactions;
+  return JSON.stringify(transactions);
 }
