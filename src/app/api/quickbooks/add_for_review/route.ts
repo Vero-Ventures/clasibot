4468b90631923e529c74bce2db460a25ac3f2ab4
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-09-26 15:03:42 -0700

    Deployment fixes and in line documentation.

--- src/app/api/quickbooks/add_for_review/route.ts
+++ src/app/api/quickbooks/add_for_review/route.ts
@@ -6,15 +6,24 @@ import { addForReview } from '@/actions/quickbooks/add-for-review';
 // import type { ForReviewTransaction } from '@/types/ForReviewTransaction';
 
 export async function GET() {
-// updatedTransaction: ForReviewTransaction,
-// classificationId: string,
-// taxCodeId: string
-  const updatedTransaction = JSON.parse((await getForReview('144')).detail);
+  // updatedTransaction: ForReviewTransaction,
+  // classificationId: string,
+  // taxCodeId: string
 
-  const result = await addForReview(updatedTransaction[1][1], '129', '3');
-  if (result.result === 'Success' && result.detail) {
-    return Response.json(JSON.parse(result.detail));
-  } else {
-    return Response.json(result);
+  // Get the for review transactions from a specific account for testing.
+  const updatedTransaction = await getForReview('144');
+
+  // Check that the transaction fetch was a success and the detail results exist.
+  if (updatedTransaction.result === 'Success' && updatedTransaction.detail) {
+    // Parse the detail results and get the index of the testing "for review" transaction object.
+    const updateTransactionData = JSON.parse(updatedTransaction.detail)[1][1];
+    // Pass the update transaction data alongside a test accountId and taxCodeId.
+    const result = await addForReview(updateTransactionData, '129', '3');
+    // Check the result and if the detail is present and return a value accordingly.
+    if (result.result === 'Success' && result.detail) {
+      return Response.json(JSON.parse(result.detail));
+    } else {
+      return Response.json(result);
+    }
   }
 }
