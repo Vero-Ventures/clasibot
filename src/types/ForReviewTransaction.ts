37a66ee7f21bedf7cac250a517d5b6b78af3a278
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-09-26 14:22:17 -0700

    Add values to track potential ID for transaction payee

--- src/types/ForReviewTransaction.ts
+++ src/types/ForReviewTransaction.ts
@@ -14,6 +14,7 @@ export type ForReviewTransaction = {
   addAsQboTxn: {
     txnTypeId: string;
     txnFdmName: string;
+    nameId: string | null;
   };
 };
 
@@ -47,6 +48,7 @@ export type ForReviewTransactionUpdateObject = {
               taxCodeId: string;
             },
           ];
+          nameId: string | null;
           txnDate: string;
           txnTypeId: string;
         };
