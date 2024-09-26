8e5dd8af1f8a1fc1e2c23d6eddf57b100fcafee9
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-09-26 14:44:25 -0700

    Commented out passed values for testing purposes until further changes.

--- src/app/api/quickbooks/add_for_review/route.ts
+++ src/app/api/quickbooks/add_for_review/route.ts
@@ -1,18 +1,21 @@
 /**
  * Defines a test API route for making sql request to the QuickBooks API 'query' endpoint.
  */
+import { getForReview } from '@/actions/quickbooks/get-for-review';
 import { addForReview } from '@/actions/quickbooks/add-for-review';
-import type { ForReviewTransaction } from '@/types/ForReviewTransaction';
+// import type { ForReviewTransaction } from '@/types/ForReviewTransaction';
 
-export async function POST(
-  updatedTransaction: ForReviewTransaction,
-  classificationId: string,
-  taxCodeId: string
+export async function GET(
+  // updatedTransaction: ForReviewTransaction,
+  // classificationId: string,
+  // taxCodeId: string
 ) {
+  const updatedTransaction = JSON.parse((await getForReview('144')).detail)
+
   const result = await addForReview(
-    updatedTransaction,
-    classificationId,
-    taxCodeId
+    updatedTransaction[1][1],
+    '129',
+    '3'
   );
   if (result.result === 'Success') {
     return Response.json(JSON.parse(result.detail));
