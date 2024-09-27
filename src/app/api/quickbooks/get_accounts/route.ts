a7a7078fdef5073ecaa51f838ec3ab68369043f6
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-09-27 10:47:55 -0700

    Update to fetch expense as test API endpoint.

--- src/app/api/quickbooks/get_accounts/route.ts
+++ src/app/api/quickbooks/get_accounts/route.ts
@@ -5,7 +5,7 @@ import { getAccounts } from '@/actions/quickbooks/get-accounts';
 
 export async function GET() {
   // Call server action to get user accounts using QuickBooks API.
-  const accounts = await getAccounts();
+  const accounts = await getAccounts('Expense');
   // Return the accounts as a JSON object.
   return Response.json(JSON.parse(accounts));
 }
