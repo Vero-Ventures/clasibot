a02e556d7e29e982417dc0aa0610ecb05d43e9d5
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-10-02 15:58:00 -0700

    Implement possibility of null sub-location.,

--- src/types/CompanyInfo.ts
+++ src/types/CompanyInfo.ts
@@ -2,6 +2,8 @@
  * Tracks the important info from the user company in a single object.
  */
 
+import type { Locations } from '@/enums/taxes';
+
 export type CompanyInfo = {
   // The company name. If the check for its value fails, saved as 'Error: Name not found'.
   name: string;
@@ -13,6 +15,6 @@ export type CompanyInfo = {
     // Can either be full name capitalized, 3 Letter Standard abbreviations, or other such as 'CA'.
     Country: string;
     // For CA country, should always be one of the values defined in the Locations Enum in the Taxes.ts file.
-    Location: string;
+    Location: Locations | null;
   };
 };
