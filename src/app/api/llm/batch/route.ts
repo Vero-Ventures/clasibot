509cb0a923f25005ac8ca6bdf47c664799bd352e
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-10-03 11:43:01 -0700

    Update API testing endpoint for new prediction methods.

--- src/app/api/llm/batch/route.ts
+++ src/app/api/llm/batch/route.ts
@@ -1,5 +1,8 @@
-import { batchQueryLLM } from '@/actions/llm-prediction/llm';
-import type { CategorizedResult } from '@/types/Category';
+import {
+  batchQueryCategoriesLLM,
+  batchQueryTaxCodesLLM,
+} from '@/actions/llm-prediction/llm';
+import type { ClassifiedResult } from '@/types/Classification';
 import type { CompanyInfo } from '@/types/CompanyInfo';
 import type { FormattedForReviewTransaction } from '@/types/ForReviewTransaction';
 
@@ -9,18 +12,33 @@ export async function POST(req: Request) {
     const body = await req.json();
     const transactions: FormattedForReviewTransaction[] =
       body.transactions || [];
-    const categories = body.categories;
     const companyInfo: CompanyInfo = body.companyInfo;
+    const categories = body.categories;
+    const type: string = body.type;
 
-    // Query the LLM model with the provided transactions and categories.
-    const results: CategorizedResult[] = await batchQueryLLM(
-      transactions,
-      categories,
-      companyInfo
-    );
-
-    // Return the results as a JSON object and a success status of 200.
-    return Response.json(results, { status: 200 });
+    if (type === 'category') {
+      const results: ClassifiedResult[] = await batchQueryCategoriesLLM(
+        transactions,
+        categories,
+        companyInfo
+      );
+      return Response.json(results, { status: 200 });
+    } else if (type === 'tax code') {
+      const taxCodes = body.taxCodes;
+      const results: ClassifiedResult[] = await batchQueryTaxCodesLLM(
+        transactions,
+        categories,
+        taxCodes,
+        companyInfo
+      );
+      return Response.json(results, { status: 200 });
+    } else {
+      // Catch any errors and return them with a bad request response using the status 400.
+      return Response.json(
+        { error: 'Error, invalid query type' },
+        { status: 400 }
+      );
+    }
   } catch (error) {
     // Catch any errors and return them with a bad request response using the status 400.
     return Response.json(
