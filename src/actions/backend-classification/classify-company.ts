39481b548fe925636ec5d82a6607af5fc7c81acc
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-10-07 14:28:58 -0700

    Create backend methods to get, classify, and save for review transactions.

---
+++ src/actions/backend-classification/classify-company.ts
@@ -0,0 +1,249 @@
+'use server';
+import { classifyTransactions } from '../classify';
+import { getAccounts } from '../quickbooks/get-accounts';
+import { getForReview } from '../quickbooks/get-for-review';
+import { getPastTransactions } from '../quickbooks/get-transactions';
+import {
+  getCompanyIndustry,
+  getCompanyLocation,
+  getCompanyName,
+} from '../quickbooks/user-info';
+import { addForReviewTransaction } from '../transaction-database/add-review-transactions';
+import type { Session } from 'next-auth/core/types';
+import type { Account } from '@/types/Account';
+import type { ClassifiedElement } from '@/types/Classification';
+import type { CompanyInfo } from '@/types/CompanyInfo';
+import type { QueryResult } from '@/types/QueryResult';
+import type {
+  ForReviewTransaction,
+  FormattedForReviewTransaction,
+  ClassifiedForReviewTransaction,
+} from '@/types/ForReviewTransaction';
+import type { Transaction } from '@/types/Transaction';
+
+export async function classifyCompany(
+  fetchToken: string,
+  session: Session
+): Promise<QueryResult> {
+  // Define the realmId (company ID) from the passed session.
+  // Assert it is not null as null sessions would not be passed.
+  const companyId = session.realmId!;
+
+  // Get the for review transactions for all the companies accounts.
+  const forReviewResult = await getForReviewTransactions(
+    session,
+    companyId,
+    fetchToken
+  );
+
+  // Check if the for review transaction call encountered an error.
+  if ('result' in forReviewResult) {
+    // If an error is encountered, return the query response.
+    return forReviewResult;
+  }
+
+  // If an error was not encountered, define the fetched for review transactions.
+  const forReviewTransactions = forReviewResult as (
+    | ForReviewTransaction
+    | FormattedForReviewTransaction
+  )[][];
+
+  // Get the users previously classified transactions to use as context for prediction.
+  const classifiedPastTransactions = await getClassifiedPastTransactions();
+
+  // Define just the formatted transactions to use in classification.
+  const formattedForReviewTransactions = forReviewTransactions.map(
+    (subArray) => subArray[0] as FormattedForReviewTransaction
+  );
+
+  // Get company info to be used in assisting with classification.
+  const companyInfo = await getCompanyInfo();
+
+  // Classify the transactions that are not uncategorized.
+  const classificationResults:
+    | Record<
+        string,
+        {
+          category: ClassifiedElement[] | null;
+          taxCode: ClassifiedElement[] | null;
+        }
+      >
+    | { error: string } = await classifyTransactions(
+    classifiedPastTransactions,
+    formattedForReviewTransactions,
+    companyInfo
+  );
+
+  // If there was an error classifing the transactions, return it as a object.
+  if (classificationResults.error) {
+    const resultDetail = classificationResults.error as string;
+    return {
+      result: 'Error',
+      message: 'Error classifying the "For Review" transactions',
+      detail: resultDetail,
+    };
+  } else {
+    // Define an version of the classification results without the possible error value.
+    const validClassificationResults = classificationResults as Record<
+      string,
+      {
+        category: ClassifiedElement[] | null;
+        taxCode: ClassifiedElement[] | null;
+      }
+    >;
+    // If there was no error classifing the transactions, use the results to create classified 'for review' transactions.
+    const classifiedForReviewTransactions =
+      createClassifiedForReviewTransactions(
+        forReviewTransactions,
+        validClassificationResults
+      );
+
+    // Save the classified transactions to the database.
+    // Return a Query Result created by the database adding method.
+    return await addForReviewTransaction(
+      classifiedForReviewTransactions,
+      companyId
+    );
+  }
+}
+
+async function getForReviewTransactions(
+  session: Session,
+  companyId: string,
+  fetchToken: string
+): Promise<
+  (ForReviewTransaction | FormattedForReviewTransaction)[][] | QueryResult
+> {
+  // Get all accounts that may contain transactions
+  const foundAccounts = await getAccounts('transaction', session);
+  const userAccounts: Account[] = JSON.parse(foundAccounts);
+
+  // Define array to hold found 'for review' transactions.
+  let foundTransactions: (
+    | ForReviewTransaction
+    | FormattedForReviewTransaction
+  )[][] = [];
+
+  // Iterate through the user accounts.
+  for (const account of userAccounts) {
+    // Get any 'for review' transactions for the current account.
+    const result = await getForReview(account.id, companyId, fetchToken);
+    // If the fetch was successful, append the resulting array to array of found transactions.
+    if (result.result === 'Success') {
+      // Parse and define the result details and concatenate them onto the current array of transactions.
+      const resultTransactions: (
+        | ForReviewTransaction
+        | FormattedForReviewTransaction
+      )[][] = JSON.parse(result.detail);
+      foundTransactions = foundTransactions.concat(resultTransactions);
+    } else {
+      return result;
+    }
+  }
+
+  // Return the found 'for review' transactions.
+  return foundTransactions;
+}
+
+async function getClassifiedPastTransactions(): Promise<Transaction[]> {
+  // Get a reference for the current date and the date 5 years ago.
+  const today = new Date();
+  const setBackRange = 5;
+  const fiveYearsAgo = new Date(
+    today.getFullYear() - setBackRange,
+    today.getMonth(),
+    today.getDate()
+  );
+
+  // Convert the dates to start date and end date strings in the format 'YYYY-MM-DD'.
+  const startDate = today.toISOString().split('T')[0];
+  const endDate = fiveYearsAgo.toISOString().split('T')[0];
+
+  // Get the past transactions from QuickBooks for checking matches.
+  const response = await getPastTransactions(startDate, endDate);
+  const pastTransactions = JSON.parse(response);
+  // Return transactions with result value removed.
+  // On failure, result is still a valid empty array.
+  return pastTransactions.slice(1);
+}
+
+async function getCompanyInfo(): Promise<CompanyInfo> {
+  // Get the key company info values from the user-info calls.
+  const userCompanyName = await getCompanyName();
+  const userCompanyIndustry = await getCompanyIndustry();
+  const userCompanyLocation = await getCompanyLocation();
+  // Return the formatted company info values.
+  return {
+    name: userCompanyName,
+    industry: userCompanyIndustry,
+    location: JSON.parse(userCompanyLocation),
+  };
+}
+
+function createClassifiedForReviewTransactions(
+  forReviewTransactions: (
+    | FormattedForReviewTransaction
+    | ForReviewTransaction
+  )[][],
+  result: Record<
+    string,
+    {
+      category: ClassifiedElement[] | null;
+      taxCode: ClassifiedElement[] | null;
+    }
+  >
+): (ForReviewTransaction | ClassifiedForReviewTransaction)[][] {
+  // Create an array for the newly classified 'for review' transactions.
+  const newCategorizedTransactions: (
+    | ClassifiedForReviewTransaction
+    | ForReviewTransaction
+  )[][] = [];
+
+  // Iterate through the selected rows and add the categorized transactions to the array.
+  for (const transaction of forReviewTransactions) {
+    // Define a value for the result asserting it is not null (confirmed by above check.)
+    const nonNullResult = result as Record<
+      string,
+      {
+        category: ClassifiedElement[] | null;
+        taxCode: ClassifiedElement[] | null;
+      }
+    >;
+    // Define the formatted value of the array.
+    const formattedTransaction =
+      transaction[0] as FormattedForReviewTransaction;
+    const fullTransaction = transaction[1] as ForReviewTransaction;
+
+    // Define values for the category and tax code predictions as nulls to be overwritten if present in results.
+    let categoryClassification = null;
+    let taxCodeClassification = null;
+
+    // Check the results to see if the predictions are valid.
+    const transactionClassifications =
+      nonNullResult[formattedTransaction.transaction_ID];
+
+    // If the results for the transaction are present, update the classification values.
+    if (transactionClassifications) {
+      categoryClassification = transactionClassifications.category;
+      taxCodeClassification = transactionClassifications.taxCode;
+    }
+
+    // Define the formatted transaction from the dual "For Review" transaction array.
+    newCategorizedTransactions.push([
+      {
+        transaction_ID: formattedTransaction.transaction_ID,
+        name: formattedTransaction.name,
+        date: formattedTransaction.date,
+        account: formattedTransaction.account,
+        accountName: formattedTransaction.accountName,
+        amount: formattedTransaction.amount,
+        // Get the categories from the result object using its ID. Gets an empty array if no match is found.
+        categories: categoryClassification,
+        taxCodes: taxCodeClassification,
+      },
+      fullTransaction,
+    ]);
+  }
+
+  return newCategorizedTransactions;
+}
---
+++ src/actions/transaction-database/add-review-transactions.ts
@@ -0,0 +1,234 @@
+'use server';
+import { db } from '@/db/index';
+import {
+  ForReviewTransaction as DatabaseForReviewTransaction,
+  ForReviewTransactionToCategories,
+  ForReviewTransactionToTaxCodes,
+  Category,
+  TaxCode,
+} from '@/db/schema';
+import type { ClassifiedElement } from '@/types/Classification';
+import type {
+  ForReviewTransaction,
+  ClassifiedForReviewTransaction,
+} from '@/types/ForReviewTransaction';
+import type { QueryResult } from '@/types/QueryResult';
+import { eq } from 'drizzle-orm';
+
+export async function addForReviewTransaction(
+  transactions: (ForReviewTransaction | ClassifiedForReviewTransaction)[][],
+  companyId: string
+): Promise<QueryResult> {
+  try {
+    for (const transaction of transactions) {
+      // Define the type of the 'for review' transaction being saved.
+      const classifiedTransaction =
+        transaction[0] as ClassifiedForReviewTransaction;
+
+      // Define the type of the raw for review transaction with key data for later QBO updates.
+      const rawTransaction = transaction[0] as ForReviewTransaction;
+
+      // Check if this transaction has already been saved into the database.
+      // Checks for transactions where the company ID and transaction ID match the current company and the ID of the current transaction.
+      const matchingTransactions = await db
+        .select()
+        .from(DatabaseForReviewTransaction)
+        .where(
+          eq(DatabaseForReviewTransaction.companyId, companyId) &&
+            eq(
+              DatabaseForReviewTransaction.transactionId,
+              classifiedTransaction.transaction_ID
+            )
+        );
+
+      // If no matching transaction is found continue addition to the database.
+      if (matchingTransactions.length === 0) {
+        // Define the object to save to the database.
+        const databaseObject = {
+          companyId: companyId,
+          transactionId: classifiedTransaction.transaction_ID,
+          accountId: classifiedTransaction.account,
+          accountName: classifiedTransaction.accountName,
+          description: rawTransaction.description,
+          origDescription: rawTransaction.origDescription,
+          date: rawTransaction.olbTxnDate,
+          amount: classifiedTransaction.amount,
+          acceptType: rawTransaction.acceptType,
+          payeeNameId: rawTransaction.addAsQboTxn.nameId,
+          transactionTypeId: rawTransaction.addAsQboTxn.txnTypeId,
+          approved: false,
+        };
+
+        // Create the new transaction in the database using the defined values.
+        const newTransaction = await db
+          .insert(DatabaseForReviewTransaction)
+          .values(databaseObject)
+          .returning();
+
+        // Create variables to track results of the classification connection methods.
+        let categoryConnectionResult = '';
+        let taxCodeConnectionResult = '';
+
+        // Check if category classifications are present.
+        // If they are, call a method to add connection to categorizations where needed.
+        if (classifiedTransaction.categories) {
+          categoryConnectionResult = await handleCategoryConnections(
+            newTransaction[0].id,
+            classifiedTransaction.categories
+          );
+        }
+
+        // If a category connection failed, log the error.
+        if (categoryConnectionResult !== 'Success') {
+          console.error(categoryConnectionResult);
+        }
+
+        // Preform the same process for the transactions tax codes.
+        if (classifiedTransaction.taxCodes) {
+          taxCodeConnectionResult = await handleTaxCodeConnections(
+            newTransaction[0].id,
+            classifiedTransaction.taxCodes
+          );
+        }
+
+        // If a tax code connection failed, log the error.
+        if (taxCodeConnectionResult !== 'Success') {
+          console.error(taxCodeConnectionResult);
+        }
+      }
+    }
+    return {
+      result: 'Success',
+      message: 'Adding "For Review" transactions to database was successful.',
+      detail:
+        '"For Review" transactions and their connections successfully added to the database.',
+    };
+  } catch (error) {
+    // Catch any errors, return the message if there is one, otherwise return a custom error message.
+    if (error instanceof Error && error.message) {
+      return {
+        result: 'Error',
+        message:
+          'An error was encountered adding a "For Review" transaction or one of its connections.',
+        detail: error.message,
+      };
+    } else {
+      return {
+        result: 'Error',
+        message:
+          'An error was encountered adding a "For Review" transaction or one of its connections.',
+        detail: 'Unknown error encountered.',
+      };
+    }
+  }
+}
+
+async function handleCategoryConnections(
+  newTransactionId: string,
+  newTransactionCategories: ClassifiedElement[]
+): Promise<string> {
+  try {
+    // Get the existing categories to check against the transactions classifications.
+    // Needed if LLM prediction for basic category type.
+    const existingCategories = await db.select().from(Category);
+
+    // Iterate through the passed categories.
+    for (const category of newTransactionCategories) {
+      // Check if the category already exists.
+      const existingCategory = existingCategories.find(
+        (dbCategory) => dbCategory.category === category.name
+      );
+
+      // Create a value to store either the found category ID or the new category ID.
+      let databaseCategoryId = 0;
+
+      // If the category does not exist, create it in the database.
+      if (!existingCategory) {
+        // Count is set to 0, as no confirmed classification for this category exists yet.
+        // If the user chooses to save with this categorization, count will be incremented in review page save function.
+        const newCategory = await db
+          .insert(Category)
+          .values({
+            category: category.name,
+            count: 0,
+          })
+          .returning();
+
+        // Store the ID of the new category.
+        databaseCategoryId = newCategory[0].id;
+      } else {
+        // If the category already exists, save its ID.
+        databaseCategoryId = existingCategory.id;
+      }
+
+      // Create the connection between the category and transaction.
+      await db.insert(ForReviewTransactionToCategories).values({
+        transactionId: newTransactionId,
+        categoryId: databaseCategoryId,
+      });
+    }
+    return 'Success';
+  } catch (error) {
+    // Catch any errors, return the message if there is one, otherwise return a custom error message.
+    if (error instanceof Error && error.message) {
+      return error.message;
+    } else {
+      return 'Error, creating the category connection for a transaction failed';
+    }
+  }
+}
+
+async function handleTaxCodeConnections(
+  newTransactionId: string,
+  newTransactionTaxCodes: ClassifiedElement[]
+): Promise<string> {
+  try {
+    // Get the existing tax codes to check against the transactions classifications.
+    // Needed if LLM prediction for the tax code type.
+    const existingTaxCodes = await db.select().from(TaxCode);
+
+    // Iterate through the passed tax codes.
+    for (const taxCode of newTransactionTaxCodes) {
+      // Check if the tax code already exists.
+      const existingTaxCode = existingTaxCodes.find(
+        (dbTaxCode) => dbTaxCode.taxCode === taxCode.name
+      );
+
+      // Create a value to store either the found tax code ID or the new tax code ID.
+      let databaseTaxCodeId = 0;
+
+      // If the tax code does not exist, create it in the database.
+      if (!existingTaxCode) {
+        // Count is set to 0, as no confirmed classification for this tax code exists yet.
+        // If the user chooses to save with this categorization, count will be incremented in review page save function.
+        const newTaxCode = await db
+          .insert(TaxCode)
+          .values({
+            taxCode: taxCode.name,
+            count: 0,
+          })
+          .returning();
+
+        // Store the ID of the new tax code.
+        databaseTaxCodeId = newTaxCode[0].id;
+      } else {
+        // If the tax code already exists, save its ID.
+        databaseTaxCodeId = existingTaxCode.id;
+      }
+
+      // Create the connection between the tax code and transaction.
+      await db.insert(ForReviewTransactionToTaxCodes).values({
+        transactionId: newTransactionId,
+        taxCodeId: databaseTaxCodeId,
+      });
+    }
+    return 'Success';
+  } catch (error) {
+    // Catch any errors, return the message if there is one, otherwise return a custom error message.
+    if (error instanceof Error && error.message) {
+      return error.message;
+    } else {
+      return 'Error, creating the category connection for a transaction failed';
+    }
+  }
+}
