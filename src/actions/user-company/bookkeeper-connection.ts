bc5c79170e4106258f74fff05d0f23c8f49a75a1
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-10-09 14:03:02 -0700

    Temp comment out code while testing other features.

--- src/actions/user-company/bookkeeper-connection.ts
+++ src/actions/user-company/bookkeeper-connection.ts
@@ -1,67 +1,104 @@
-'use server';
-import { db } from '@/db/index';
-import { Company, User } from '@/db/schema';
-import { eq } from 'drizzle-orm';
-import { getServerSession } from 'next-auth';
-import { options } from '@/app/api/auth/[...nextauth]/options';
-import { getCompanyName, getCompanyIndustry } from '../quickbooks/user-info';
+// 'use server';
+// import { db } from '@/db/index';
+// import { Company, User } from '@/db/schema';
+// import { eq } from 'drizzle-orm';
+// import { getServerSession } from 'next-auth';
+// import { options } from '@/app/api/auth/[...nextauth]/options';
+// import { getCompanyName, getCompanyIndustry } from '../quickbooks/user-info';
+// import type { QueryResult } from '@/types/QueryResult';
 
-export async function checkCompanyConnection(): Promise<boolean> {
-  // Get the company identifing info from the session.
-  const session = await getServerSession(options);
-  const realmId = session?.realmId;
-  // Check the identifing info could be found.
-  if (realmId) {
-    // Check that the company with that realmId exists in the database.
-    const currentCompany = await db
-      .select()
-      .from(Company)
-      .where(eq(Company.realmId, realmId));
-    // If a company was found, update its values then return its connection status.
-    if (currentCompany.length > 0) {
-      await updateCompanyInfo(realmId);
-      return currentCompany[0].bookkeeperConnected;
-    } else {
-      // If no company is found, the connection is false.
-      return false;
-    }
-  } else {
-    // If realm ID could not be found, return false.
-    return false;
-  }
-}
+// export async function checkCompanyConnection(): Promise<boolean> {
+//   // Get the company identifing info from the session.
+//   const session = await getServerSession(options);
+//   const realmId = session?.realmId;
+//   // Check the identifing info could be found.
+//   if (realmId) {
+//     // Check that the company with that realmId exists in the database.
+//     const currentCompany = await db
+//       .select()
+//       .from(Company)
+//       .where(eq(Company.realmId, realmId));
+//     // If a company was found, update its values then return its connection status.
+//     if (currentCompany.length > 0) {
+//       await updateCompanyInfo(realmId);
+//       return currentCompany[0].bookkeeperConnected;
+//     } else {
+//       // If no company is found, the connection is false.
+//       return false;
+//     }
+//   } else {
+//     // If realm ID could not be found, return false.
+//     return false;
+//   }
+// }
 
-async function updateCompanyInfo(realmId: string) {
-  // Get the current company from the database.
-  const currentCompany = await db
-    .select()
-    .from(Company)
-    .where(eq(Company.realmId, realmId));
-  // If a company was found, update its values then return its connection status.
-  if (currentCompany.length > 0) {
-    // Define the first company as the company as unique check ensure only ever 0 or 1 values are returned.
-    const updateCompany = currentCompany[0];
+// async function updateCompanyInfo(realmId: string) {
+//   // Get the current company from the database.
+//   const currentCompany = await db
+//     .select()
+//     .from(Company)
+//     .where(eq(Company.realmId, realmId));
+//   // If a company was found, update its values then return its connection status.
+//   if (currentCompany.length > 0) {
+//     // Define the first company as the company as unique check ensure only ever 0 or 1 values are returned.
+//     const updateCompany = currentCompany[0];
 
-    // Get the company name and update the company object.
-    const companyName = await getCompanyName();
-    if (companyName !== 'Error: Name not found') {
-      updateCompany.name = companyName;
-    }
+//     // Get the company name and update the company object.
+//     const companyName = await getCompanyName();
+//     if (companyName !== 'Error: Name not found') {
+//       updateCompany.name = companyName;
+//     }
 
-    // Get the company name and update the company object.
-    const companyIndustry = await getCompanyIndustry();
-    if (companyName !== 'Error: Name not found') {
-      updateCompany.industry = companyIndustry;
-    }
+//     // Get the company name and update the company object.
+//     const companyIndustry = await getCompanyIndustry();
+//     if (companyName !== 'Error: Name not found') {
+//       updateCompany.industry = companyIndustry;
+//     }
 
-    // Update the database with the updated company object.
-    await db
-      .update(Company)
-      .set(updateCompany)
-      .where(eq(Company.realmId, realmId));
-  }
-}
+//     // Update the database with the updated company object.
+//     await db
+//       .update(Company)
+//       .set(updateCompany)
+//       .where(eq(Company.realmId, realmId));
+//   }
+// }
 
-export async function addCompanyConnection(): Promise<> {}
+// export async function addCompanyConnection(
+//   userEmail: string,
+//   companyName: string
+// ): Promise<QueryResult> {
+//   // Use the email to get the user from the database for their company ID.
+//   const databaseUser = await db
+//     .select()
+//     .from(User)
+//     .where(eq(User.email, userEmail));
+//   if (databaseUser) {
+//     // Check the company exists in the database using the user ID and company name as unique identifier.
+//     const databaseCompany = await db
+//       .select()
+//       .from(Company)
+//       .where(
+//         eq(Company.userId, databaseUser[0].id) && eq(Company.name, companyName)
+//       );
+//     if (databaseCompany) {
+//       // Update the
+//     } else {
+//       return {
+//         result: 'Error',
+//         message: 'Company could not be found.',
+//         detail:
+//           'No Companies with that name were found belonging to the user with that email.',
+//       };
+//     }
+//   } else {
+//     return {
+//       result: 'Error',
+//       message: 'User could not be found.',
+//       detail: 'No Users in the database with that email could be found.',
+//     };
+//   }
+// }
 
-export async function removeCompanyConnection(): Promise<> {}
+// export async function removeCompanyConnection(): Promise<> {}
+
+// export async function removeAccounantCompanyConnection(): Promise<> {}
