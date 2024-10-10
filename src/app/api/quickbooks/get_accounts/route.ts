8a3fdb420243e8b87da2d4445036a33befdd5773
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-10-10 09:01:41 -0700

    Undo test changes.

--- src/app/api/quickbooks/get_accounts/route.ts
+++ src/app/api/quickbooks/get_accounts/route.ts
@@ -1,13 +1,13 @@
 /**
  * Defines a test API route for getting user accounts using QuickBooks API.
  */
-import { getIsAccounant } from '@/actions/quickbooks/user-info'
+import { getAccounts } from '@/actions/quickbooks/get-accounts';
 
 export async function GET() {
   // Call server action to get user accounts using QuickBooks API.
   // Call with either expense or transaction depending on account type needed.
   // Expense for possible classification accounts. Transaction for accounts that may contain 'for review' transactions.
-  const accounts = await getIsAccounant();
+  const accounts = await getAccounts('Expense');
   // Return the accounts as a JSON object.
-  return Response.json(accounts);
+  return Response.json(JSON.parse(accounts));
 }
