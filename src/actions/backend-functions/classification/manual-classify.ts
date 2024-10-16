fb4024be6421e7fa690962c4cf11c266ba25e9ce
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-10-16 09:26:46 -0700

    Comment out code until ready for integration with synthetic login.

--- src/actions/backend-functions/classification/manual-classify.ts
+++ src/actions/backend-functions/classification/manual-classify.ts
@@ -1,31 +1,36 @@
-'use server';
-import { classifyCompany } from './classify-company';
-import type { Session } from 'next-auth/core/types';
+// 'use server';
+// import { classifyCompany } from './classify-company';
+// // import type { Session } from 'next-auth/core/types';
 
-export async function manualClassify(
-  setManualReviewState: (newState: string) => void
-): Promise<boolean> {
-  // Make some call to synthetic login to get the session and related tokens.
-  const fetchToken = null;
-  const authId = null;
-  const session = null;
+// export async function manualClassify(
+//   setManualReviewState: (newState: string) => void
+// ): Promise<boolean> {
+//   // Make some call to synthetic login to get the session and related tokens.
+//   //
+//   //
 
-  // Make a call to the company classification method with the retrived values.
-  const result = await classifyCompany(
-    fetchToken,
-    authId,
-    session,
-    true,
-    setManualReviewState
-  );
+//   const fetchToken = null;
+//   const authId = null;
+//   const session = null;
 
-  if (result.result === 'Success') {
-    setManualReviewState('Classifications Saved.');
-    return true;
-  } else {
-    console.error('Unexpected Error, Message:' + result.message);
-    console.error('Error Details: ' + result.detail);
-    setManualReviewState('An Unexpected Error Occured');
-    return false;
-  }
-}
+//   // Make a call to the company classification method with the retrived values.
+//   const result = await classifyCompany(
+//     fetchToken,
+//     authId,
+//     session,
+//     true,
+//     setManualReviewState
+//   );
+
+//   // Depending on the result of the classification call, update the manual review state and return a success boolean.
+//   if (result.result === 'Success') {
+//     setManualReviewState('Classifications Saved.');
+//     return true;
+//   } else {
+//     // Log the errors encountered that resulted in failure.
+//     console.error('Unexpected Error, Message:' + result.message);
+//     console.error('Error Details: ' + result.detail);
+//     setManualReviewState('An Unexpected Error Occured');
+//     return false;
+//   }
+// }
