47811ecfca34a5aa703057b04c382865d67ceab0
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-10-16 09:35:20 -0700

    Update manual classify to non-error causing version to be updated later.

--- src/actions/backend-functions/classification/manual-classify.ts
+++ src/actions/backend-functions/classification/manual-classify.ts
@@ -1,36 +1,47 @@
-// 'use server';
-// import { classifyCompany } from './classify-company';
-// // import type { Session } from 'next-auth/core/types';
+'use server';
+import { classifyCompany } from './classify-company';
+// import type { Session } from 'next-auth/core/types';
+import { getServerSession } from 'next-auth';
+import { options } from '@/app/api/auth/[...nextauth]/options';
 
-// export async function manualClassify(
-//   setManualReviewState: (newState: string) => void
-// ): Promise<boolean> {
-//   // Make some call to synthetic login to get the session and related tokens.
-//   //
-//   //
+export async function manualClassify(
+  setManualReviewState: (newState: string) => void
+): Promise<boolean> {
+  // Make some call to synthetic login to get the session and related tokens.
+  //
+  //
 
-//   const fetchToken = null;
-//   const authId = null;
-//   const session = null;
+  const fetchToken = 'null';
+  const authId = 'null';
+  // Temp usage of get session to be replaced later by synthetic call.
+  const session = await getServerSession(options);
 
-//   // Make a call to the company classification method with the retrived values.
-//   const result = await classifyCompany(
-//     fetchToken,
-//     authId,
-//     session,
-//     true,
-//     setManualReviewState
-//   );
+  // Only continue if a valid session is found.
+  if (session) {
+    // Make a call to the company classification method with the retrived values.
+    const result = await classifyCompany(
+      fetchToken,
+      authId,
+      session,
+      true,
+      setManualReviewState
+    );
 
-//   // Depending on the result of the classification call, update the manual review state and return a success boolean.
-//   if (result.result === 'Success') {
-//     setManualReviewState('Classifications Saved.');
-//     return true;
-//   } else {
-//     // Log the errors encountered that resulted in failure.
-//     console.error('Unexpected Error, Message:' + result.message);
-//     console.error('Error Details: ' + result.detail);
-//     setManualReviewState('An Unexpected Error Occured');
-//     return false;
-//   }
-// }
+    // Depending on the result of the classification call, update the manual review state and return a success boolean.
+    if (result.result === 'Success') {
+      setManualReviewState('Classifications Saved.');
+      return true;
+    } else {
+      // Log the errors encountered that resulted in failure.
+      console.error('Unexpected Error, Message:' + result.message);
+      console.error('Error Details: ' + result.detail);
+      setManualReviewState('An Unexpected Error Occured');
+      return false;
+    }
+  } else {
+    // Log the errors and update to failure state, then return false for success boolean.
+    console.error('Session Not Found.');
+    setManualReviewState('An Unexpected Error Occured');
+    return false;
+  }
+}
