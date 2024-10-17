a6281ecee95ac8aeb610214c879f0d2da3806d29
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-10-17 15:41:13 -0700

    Documentation and code refactoring updates.

--- src/actions/quickbooks/get-saved-transactions.ts
+++ src/actions/quickbooks/get-saved-transactions.ts
@@ -9,8 +9,9 @@ import type { ErrorResponse } from '@/types/ErrorResponse';
 import type { TaxCode } from '@/types/TaxCode';
 import type { Transaction } from '@/types/Transaction';
 import type { Session } from 'next-auth/core/types';
+import type { QueryResult } from '@/types/QueryResult';
 
-// Get all saved transactions from the QuickBooks API.
+// Get all past saved transactions from the QuickBooks API.
 // May take a start date and end date and / or a synthetic session as optional parameters.
 // Returns: An array of objects starting with a Query Result, then containing Transaction objects.
 export async function getSavedTransactions(
@@ -54,6 +55,7 @@ export async function getSavedTransactions(
     };
 
     // Query user preferences to get the multi-currency preference for the user.
+    // Multi-Currency determines the name of the columnn that contains the transaction amount.
     const preferences: PreferenceResponse = await new Promise((resolve) => {
       qbo.getPreferences((err: ErrorResponse, data: PreferenceResponse) => {
         if (err && checkFaultProperty(err)) {
@@ -64,11 +66,11 @@ export async function getSavedTransactions(
       });
     });
 
-    // Get the current date.
+    // Get the current date to potentially use in start and end date setting..
     const today = new Date();
 
-    // If there is no start date passed as an argument, define the default start date.
-    // Current default value: 2 years ago.
+    // If no start date was passed as an argument, define a default start date.
+    // Default Balue: 2 years ago.
     if (startDate === '') {
       const TwoYearsAgo = new Date(
         today.getFullYear() - 2,
@@ -78,21 +80,21 @@ export async function getSavedTransactions(
       startDate = TwoYearsAgo.toISOString().split('T')[0];
     }
 
-    // If there is no end date passed as an argument, set the end date to the current date.
+    // If no end date was passed as an argument, set the end date to the default: the current date.
     if (endDate === '') {
       endDate = today.toISOString().split('T')[0];
     }
 
     // Defines the start date, end date, maximum returned transactions.
-    // Also defines a list of the relevant data columns to include.
+    // Columns defines a list of the data columns for each transaction to included as part of the response.
     const parameters = {
       start_date: startDate,
       end_date: endDate,
       limit: 1000,
-      columns: ['txn_type', 'name', 'account_name', 'other_account'],
+      columns: ['txn_type', 'name', 'other_account'],
     };
 
-    // Check if the user has multi-currency enabled and add the appropriate amount column to the parameters.
+    // Check MultiCurrencyEnabled and add the appropriate column that determines transaction amount to the parameters.
     if (preferences.CurrencyPrefs.MultiCurrencyEnabled) {
       parameters.columns.push('subt_nat_home_amount');
     } else {
@@ -129,115 +131,155 @@ export async function getSavedTransactions(
       );
     });
 
-    // Get the results rows from the JSON response.
-    const results = response.Rows.Row;
-
-    const formattedTransactions = [];
+    // Get the results rows from the JSON response and define a results array.
+    const responseRows = response.Rows.Row;
+    const results: (QueryResult | Transaction)[] = [];
 
     // Create a formatted Query Result object for the QBO API call then push it to the first index of the transactions array.
     const QueryResult = createQueryResult(success, error);
-    formattedTransactions.push(QueryResult);
+    results.push(QueryResult);
 
-    // If no rows were returned, skip formatting the transactions and return the result field.
-    if (results !== undefined) {
-      // Call expense accounts to check against classification.
-      const accounts = await getAccounts('Expense');
-      const accountResults = JSON.parse(accounts);
-      // Check if the call was a failure and update the query result if it is.
-      if (accountResults[0].result === 'Error') {
-        formattedTransactions[0] = {
+    // Check if transaction rows were reterned by the QuickBooks API call and the query result was not an error.
+    if (responseRows && QueryResult.result !== 'Error') {
+      // Call helper method to check and format response data and return an array of valid formatted transactions.
+      checkAndFormatTransactions(responseRows, results, session);
+    }
+
+    // Return the formatted results as a JSON string.
+    return JSON.stringify(results);
+  } catch (error) {
+    // Catch any errors and return them an stringified error Query Result inside an array to match standard formatting.
+    // Set the detail string to the error message if it is present.
+    if (error instanceof Error) {
+      return JSON.stringify([
+        {
           result: 'error',
           message:
-            'Unexpected error occured while fetching expense accounts for transaction checking.',
-          detail: accountResults[0].message,
-        };
-      } else {
-        // If the call was successful, create the parsed accounts object to be checked against.
-        const parsedAccounts: Account[] = accountResults.slice(1);
+            'Unexpected error occured while fetching saved user transactions.',
+          detail: error.message,
+        },
+      ]);
+    } else {
+      return JSON.stringify([
+        {
+          result: 'error',
+          message:
+            'Unexpected error occured while fetching saved user transactions.',
+          detail: 'N/A',
+        },
+      ]);
+    }
+  }
+}
 
-        // Iterate through the transactions to check and format them.
-        for (const transaction of results) {
-          // Check if the transaction is a summary row and skip it.
-          if (transaction.Summary) {
-            break;
-          }
-          // Define the rows of the important returned values.
-          const idRow = 0;
-          const nameRow = 1;
-          const accountRow = 2;
-          const categoryRow = 3;
-          const amountRow = 4;
+// Takes the QuickBooks response rows and a results array.
+// Also takes the session to make purchase calls for both frontend and backend calls.
+// Formats the rows response into transactions to be stored in the results array.
+// Returns: A results array containing a Query Result in the first index and the formatted Transaction objects.
+async function checkAndFormatTransactions(
+  rows: {
+    ColData: {
+      value: string | number;
+      id: string;
+    }[];
+    Summary: string;
+  }[],
+  results: (QueryResult | Transaction)[],
+  session: Session | null
+) {
+  // Call list of expense accounts to check the transaction classifications against.
+  const accounts = await getAccounts('Expense');
+  const accountResults = JSON.parse(accounts);
 
-          // Skip no-name transactions, transactions without an account, and transactions without an amount.
+  // Check if the account call was a failure and set the Query Result index of the results to an error if it was.
+  if (accountResults[0].result === 'Error') {
+    results[0] = {
+      result: 'error',
+      message:
+        'Unexpected error occured while fetching expense accounts for transaction checking.',
+      detail: accountResults[0].message,
+    };
+  } else {
+    // If the accounts call was successful, remove the Query Result from the first index and define the array of Account objects.
+    const parsedAccounts: Account[] = accountResults.slice(1);
+
+    // Iterate through the transaction rows to check and format them.
+    for (const row of rows) {
+      // Check if the row is a summary row and skip it if it is.
+      if (row.Summary) {
+        break;
+      }
+
+      // Define the index values of the important transaction values.
+      const idRow = 0;
+      const nameRow = 1;
+      const categoryRow = 2;
+      const amountRow = 3;
+
+      // Skip no-name transactions (needed for prediction matching), transactions without a category,
+      // Transactions without an amount are skiped as it ensures only expense transactions are recorded (also helps with prediction ordering).
+      if (
+        row.ColData[nameRow].value !== '' &&
+        row.ColData[categoryRow].value !== '' &&
+        Number(row.ColData[amountRow].value) < 0
+      ) {
+        // Only continue if an expense account with the same name as the category exists.
+        if (
+          parsedAccounts.some(
+            (account) => account.name === row.ColData[categoryRow].value
+          )
+        ) {
+          // Convert the values from transaction row to a Transaction object.
+          // Explicitly defined types due to values from the API being both strings and numbers.
+          const newFormattedTransaction: Transaction = {
+            name: String(row.ColData[nameRow].value),
+            amount: Number(row.ColData[amountRow].value),
+            category: String(row.ColData[categoryRow].value),
+            taxCodeName: '',
+          };
+
+          // Search for the purchase related to the transaction to get the tax rate.
+          // Pass the session to work with both frontend and backend calls.
+          const transactionPurchase = await findFormattedPurchase(
+            String(row.ColData[idRow].id),
+            session
+          );
+
+          // Get the users tax codes and parse it to a Query Result and an array of Tax Code objects.
+          const userTaxCodes = JSON.parse(await getTaxCodes(session));
+
+          // Check if fetch of either the purchase and tax codes resulted in an error.
           if (
-            transaction.ColData[nameRow].value !== '' &&
-            transaction.ColData[accountRow].value !== '' &&
-            transaction.ColData[categoryRow].value !== '' &&
-            transaction.ColData[nameRow].value !== '' &&
-            Number(transaction.ColData[amountRow].value) < 0
+            transactionPurchase.result_info.result === 'Error' ||
+            userTaxCodes[0].result === 'Error'
           ) {
-            // Check if an expense account with the same name exists and only continue if it does.
-            if (
-              parsedAccounts.some(
-                (account) =>
-                  account.name === transaction.ColData[categoryRow].value
-              )
-            ) {
-              // Reads the values from the specified columns in the current row of the results.
-              // Explicitly define the types due to values from the API being either a string or number.
-              const newFormattedTransaction: Transaction = {
-                name: String(transaction.ColData[nameRow].value),
-                amount: Number(transaction.ColData[amountRow].value),
-                category: String(transaction.ColData[categoryRow].value),
-                taxCodeName: '',
-              };
+            // Define truth values for both the purchase and tax codes to indicate if that fetch failed.
+            const purchaseError =
+              transactionPurchase.result_info.result === 'Error';
+            const taxCodeError = userTaxCodes[0].result === 'Error';
 
-              // Find the purchase related to the transaction to get its tax rate.
-              const transactionPurchase = await findFormattedPurchase(
-                String(transaction.ColData[idRow].id),
-                session
-              );
-
-              // Get the tax codes for the user to get the tax code name from the ID's.
-              const userTaxCodes = JSON.parse(await getTaxCodes());
-
-              // Check the fetch of the purchase and tax codes was successful.
-              if (
-                transactionPurchase.result_info.result === 'Error' ||
-                userTaxCodes[0].result === 'Error'
-              ) {
-                // Define truth values indicating if that fetch failed.
-                const purchaseError =
-                  transactionPurchase.result_info.result === 'Error';
-                const taxCodeError = userTaxCodes[0].result === 'Error';
-
-                // If an error occured, leave the tax code unfilled and log an error.
-                console.log(
-                  'Error getting transaction tax details, Purchase Failure? ' +
-                    purchaseError +
-                    ', Tax Code Failure? ' +
-                    taxCodeError
-                );
-              } else {
-                // Iterate through user tax codes to find the matching ID.
-                for (const taxCode of userTaxCodes.slice(1) as TaxCode[]) {
-                  if (taxCode.Id == transactionPurchase.taxCodeId) {
-                    // Record the name of the tax code with the matching ID.
-                    newFormattedTransaction.taxCodeName = taxCode.Name;
-                  }
-                }
-
-                // Add the formatted transaction to the array.
-                formattedTransactions.push(newFormattedTransaction);
+            // When an error occurs, leave the tax code unfilled and log an error.
+            console.log(
+              'Error getting transaction tax details, Purchase Failure? ' +
+                purchaseError +
+                ', Tax Code Failure? ' +
+                taxCodeError
+            );
+          } else {
+            // If both retrivals were successful, skip the Query Result and iterate through the user tax codes.
+            for (const taxCode of userTaxCodes.slice(1) as TaxCode[]) {
+              // Find the tax code that matches the one in the purchase object related to the current transaction row.
+              if (taxCode.Id == transactionPurchase.taxCodeId) {
+                // Update the formatted transaction with the tax code name.
+                newFormattedTransaction.taxCodeName = taxCode.Name;
               }
             }
+
+            // Add the formatted transaction for the current row to the array.
+            results.push(newFormattedTransaction);
           }
         }
       }
     }
-    // Return the formatted results as a JSON string.
-    return JSON.stringify(formattedTransactions);
-  } catch (error) {
-    return JSON.stringify(error);
   }
 }
--- src/actions/quickbooks/taxes.ts
+++ src/actions/quickbooks/taxes.ts
@@ -1,10 +1,10 @@
 'use server';
 import { createQBObject, createQBObjectWithSession } from '@/actions/qb-client';
 import { checkFaultProperty, createQueryResult } from './query-helpers';
-import type { Session } from 'next-auth/core/types';
-import type { TaxCode } from '@/types/TaxCode';
-import type { ErrorResponse } from '@/types/ErrorResponse';
 import { Locations, TaxCodes } from '@/enums/taxes';
+import type { ErrorResponse } from '@/types/ErrorResponse';
+import type { TaxCode } from '@/types/TaxCode';
+import type { Session } from 'next-auth/core/types';
 
 // Get all the tax codes and returns them as an array of tax code objects.
 // May take a synthetic login session to use instead of the regular session.
@@ -23,7 +23,7 @@ export async function getTaxCodes(
       qbo = await createQBObject();
     }
 
-    // Define format of returned group of tax code objects.
+    // Define format of the returned group of tax code objects.
     type TaxCodeResponse = {
       QueryResponse: { TaxCode: TaxCode[] };
       Error: {
@@ -61,22 +61,23 @@ export async function getTaxCodes(
     });
 
     // Create an array to store the tax codes.
-    const taxCodes = [];
+    const results = [];
 
     // Create a formatted Query Result object for the QBO API call then push it to the first index of the tax code array.
     const queryResult = createQueryResult(success, error);
-    taxCodes.push(queryResult);
+    results.push(queryResult);
 
-    // Iterate through the response to format and add the individal tax codes to the array.
+    // Iterate through the response to filter, format, and push the tax codes to the array.
     for (const taxCode of response.QueryResponse.TaxCode) {
-      // Ignore inactive tax codes and filter to valid tax codes by name.
+      // Ignore inactive tax codes, then filter out invalid tax codes.
       if (taxCode.Active === true && checkForCurrentTaxCode(taxCode.Name)) {
-        taxCodes.push(formatTaxCode(taxCode));
+        // Call helper method to format the tax code, then push it to the results array.
+        results.push(formatTaxCode(taxCode));
       }
     }
 
-    // Return the array of a query result and the formatted tax codes as a JSON string.
-    return JSON.stringify(taxCodes);
+    // Return the array with the Query Result and Tax Code objects as a JSON string.
+    return JSON.stringify(results);
   } catch (error) {
     // Return a query result formatted error message.
     // Include a detail string if error message is present.
@@ -96,19 +97,23 @@ export async function getTaxCodes(
   }
 }
 
-// Get an array of the tax code names used by a specific location (province or territory).
+// Finds the valid tax codes for a user by a specific location.
+// Takes a Canadian sub-location name defined by the Locations enum.
+// Returns: An array of the tax code names used by a specific location.
 export async function getTaxCodesByLocation(
   LocationName: Locations
 ): Promise<string[]> {
+  // Define the array of valid tax code name strings.
   const taxCodeNames = [];
-  // Push the nation wide tax codes that apply a 0% rate.
+
+  // Add the standard nation wide tax codes that apply a 0% rate.
   taxCodeNames.push(TaxCodes.Exempt, TaxCodes.ZeroRated, TaxCodes.OutOfScope);
 
-  // Push the nation wide GST tax code.
+  // Add the nation wide GST tax code.
   taxCodeNames.push(TaxCodes.Gst);
 
-  // Use switch case to add appropriate tax codes by location.
-  // Alberta and the territories have not additional tax codes.
+  // Use switch case to determine and add appropriate tax codes by location.
+  // Alberta and the Territories (NU, NW, YK) have not additional tax codes.
   switch (LocationName) {
     case Locations.BC:
       taxCodeNames.push(TaxCodes.GstPstBC, TaxCodes.PstBC);
@@ -143,9 +148,9 @@ export async function getTaxCodesByLocation(
   return taxCodeNames;
 }
 
-// Take a tax code response from the API and format it by grabbing the relevant values from the whole response.
+// Take a tax code response from QuickBooks and converts it to a formatted Tax Code object.
 function formatTaxCode(taxCodeResponse: TaxCode): TaxCode {
-  // Create object with relevant elements from passed raw API tax code object.
+  // Create object with relevant elements from the passed raw API tax code object.
   const formattedTaxCode: TaxCode = {
     Id: taxCodeResponse.Id,
     Name: taxCodeResponse.Name,
@@ -159,7 +164,8 @@ function formatTaxCode(taxCodeResponse: TaxCode): TaxCode {
   return formattedTaxCode;
 }
 
-// Take a tax code name and check if it currently valid for use.
+// Takes a tax code name and check if it currently valid for use.
+// Returns: Tax code validity as a boolean value.
 function checkForCurrentTaxCode(taxCodeName: string): boolean {
   // Check the tax code name against the Enum of valid tax codes.
   return (Object.values(TaxCodes) as string[]).includes(taxCodeName);
