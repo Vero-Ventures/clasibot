ef5eb926079ac338b809106b5725543fa3f31368
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-10-21 09:15:26 -0700

    Update naming to meet proper conventions

--- src/app/api/classification-timestamp/get-timestamp/route.ts
+++ src/app/api/classification-timestamp/get-timestamp/route.ts
@@ -1,7 +1,7 @@
 import { getNextReviewTimestamp } from '@/actions/backend-functions/database-functions/next-review-timestamp';
 
 export async function GET() {
-  // Get the timestamp of the next backend review from the database.
+  // Get the timestamp of the next backend classification from the database.
   const response = await getNextReviewTimestamp();
   // Return the timestamp as a JSON object.
   return Response.json(response);
--- src/app/api/classification-timestamp/set-timestamp/route.ts
+++ src/app/api/classification-timestamp/set-timestamp/route.ts
@@ -1,7 +1,7 @@
 import { setNextReviewTimestamp } from '@/actions/backend-functions/database-functions/next-review-timestamp';
 
 export async function GET() {
-  // Set the timestamp of the next backend review to be 7 days from the current date.
+  // Set the timestamp of the next backend classification to be 7 days from the current date.
   const response = await setNextReviewTimestamp();
   // Return the database timestamp as a JSON object.
   return Response.json(JSON.parse(response));
