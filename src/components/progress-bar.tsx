33699c5c9142613ddc9c73f3d31926868570ed0c
mckenzie-nicol <122124971+mckenzie-nicol@users.noreply.github.com>
2024-11-05 22:49:24 -0800

    Update progress bar to improve modularity

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -2,37 +2,14 @@ import React from 'react';
 import { CheckIcon } from 'lucide-react'; // Make sure you have lucide-react installed
 
 interface ProgressBarProps {
-  progress: string;
+  completedChunks: number;
+  maxChunks: number;
 }
 
-const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
-  const calculateChunks = (progress: string) => {
-    switch (progress) {
-      case 'Start Classify':
-        return 1;
-      case 'Synthetic Login':
-        return 2;
-      case 'Get For Review Transactions':
-        return 3;
-      case 'Get Saved Transactions':
-        return 4;
-      case 'Classify For Review Transactions':
-        return 5;
-      case 'Create New Classified Transactions':
-        return 6;
-      case 'Save New Classified Transactions':
-        return 7;
-      case 'Load New Classified Transactions':
-        return 8;
-      case 'Classify Complete':
-        return 9;
-      default:
-        return 0;
-    }
-  };
+const ProgressBar: React.FC<ProgressBarProps> = ({ completedChunks, maxChunks }) => {
 
-  const filledChunks = calculateChunks(progress);
-  const isCompleted = filledChunks === 9;
+  
+  const isCompleted = completedChunks === maxChunks;
 
   const circleCommonClasses = 'h-4 w-4 rounded-full m-1';
 
@@ -43,13 +20,13 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
           <CheckIcon className="h-8 w-8 text-white" />
         </div>
       ) : (
-        Array.from({ length: 9 }).map((_, index) => (
+        Array.from({ length: maxChunks }).map((_, index) => (
           <div
             key={index}
             className={`${circleCommonClasses} ${
-              index < filledChunks
+              index < completedChunks
                 ? 'bg-green-500'
-                : index === filledChunks
+                : index === completedChunks
                   ? 'animate-bounce bg-blue-500'
                   : 'bg-gray-300'
             }`}
