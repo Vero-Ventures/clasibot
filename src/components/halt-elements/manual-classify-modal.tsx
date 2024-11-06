541150f569e101c78820f79496281ed88d1f4a2d
mckenzie-nicol <122124971+mckenzie-nicol@users.noreply.github.com>
2024-11-05 23:58:02 -0800

    update modal to tell user about processes

--- src/components/halt-elements/manual-classify-modal.tsx
+++ src/components/halt-elements/manual-classify-modal.tsx
@@ -16,9 +16,14 @@ const ManualClassifyModal = ({
     <div
       className={`fixed left-0 top-0 flex h-full w-full items-center justify-center bg-gray-900 bg-opacity-50`}>
       <div className="mx-4 flex w-96 flex-col space-y-4 rounded-lg bg-white p-6">
-        <p id="ResultMessage" className="text-center font-medium text-gray-800">
+        <p id="ResultMessage" className="text-center font-medium text-base text-gray-800">
           {progressMessage}
         </p>
+        {completedChunks < maxChunks && (
+          <p className="text-center text-sm text-gray-800">
+            Working through process {completedChunks + 1} of {maxChunks}...
+          </p>
+        )}
         <ProgressBar completedChunks={completedChunks} maxChunks={maxChunks} />
       </div>
     </div>
