403ccf4b890607c400b82d0e3176f1755449cf90
mckenzie-nicol <122124971+mckenzie-nicol@users.noreply.github.com>
2024-11-13 13:44:58 -0800

    Update classification complete modal to show visual feedback of process Adjust spacing of elements

--- src/components/modals/manual-classify-modals.tsx
+++ src/components/modals/manual-classify-modals.tsx
@@ -1,5 +1,6 @@
 import { ProgressBar } from '@/components/loading/index';
 import { Button } from '@/components/ui/button';
+import { CheckIcon, XIcon } from 'lucide-react';
 
 interface ManualClassifyProgressProps {
   displayState: boolean;
@@ -63,14 +64,25 @@ export const ManualClassifyCompleteModal: React.FC<
           className={`fixed left-0 top-0 flex h-full w-full items-center justify-center bg-gray-900 bg-opacity-50 ${displayState ? '' : 'hidden'}`}>
           <div className="mx-4 w-96 rounded-lg bg-white p-6">
             <>
-              <h2
-                id="ResultTitle"
-                className="mb-4 text-center text-2xl font-bold text-green-500">
-                {manualClassificationState === 'Error' ? 'Error' : 'Complete'}
-              </h2>
+              <div className="flex items-center justify-center space-x-4 p-2 text-center">
+                <h2
+                  id="ResultTitle"
+                  className={`text-center text-2xl font-bold ${manualClassificationState === 'Error' ? 'text-red-500' : 'text-green-500'} `}>
+                  {manualClassificationState === 'Error' ? 'Error' : 'Complete'}
+                </h2>
+                {manualClassificationState === 'Error' ? (
+                  <div className="flex h-12 w-12 animate-failureAnimation items-center justify-center rounded-full bg-red-500">
+                    <XIcon className="h-6 w-6 text-white" />
+                  </div>
+                ) : (
+                  <div className="flex h-12 w-12 animate-successAnimation items-center justify-center rounded-full bg-green-500">
+                    <CheckIcon className="h-8 w-8 text-white" />
+                  </div>
+                )}
+              </div>
               <p
                 id="ResultMessage"
-                className="mb-6 text-center font-medium text-gray-800">
+                className="mx-6 mb-6 mt-3 text-center font-medium text-gray-800">
                 {manualClassificationState === 'Error'
                   ? 'An error occured during the classification process. Please try again later or contact us if the issue persists.'
                   : 'Your transactions are classified and ready for review.'}
@@ -81,7 +93,7 @@ export const ManualClassifyCompleteModal: React.FC<
               className="flex justify-center gap-4">
               <Button
                 id="CloseButton"
-                className="h-12 w-40 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
+                className="text-md space-x-4 rounded-md bg-gray-400 px-4 py-2 font-semibold text-gray-900 hover:bg-gray-500"
                 onClick={() => setDisplayState(false)}>
                 {manualClassificationState === 'Error' ? 'Close' : 'Continue'}
               </Button>
