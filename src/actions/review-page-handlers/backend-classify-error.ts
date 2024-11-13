ffc50f1bd96e86f756e4290aa9ad98f9613560f9
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-11-13 14:42:18 -0800

    Update function to work without using client passed callbacks.

--- src/actions/review-page-handlers/backend-classify-error.ts
+++ src/actions/review-page-handlers/backend-classify-error.ts
@@ -4,52 +4,36 @@ import {
   dismissBackendClassifyError,
 } from '@/actions/backend-actions/database-functions/index';
 
-// Checks if the user has a backend Classification error logged in the database and updates the related frontend UI element states.
-// Takes: State update methods for if a backend Classification error was found and for showing the related UI element.
-// Returns: None, uses state updaters to pass result frontend.
-export async function checkBackendClassifyErrorStatus(
-  setBackendClassifyError: (newState: boolean) => void,
-  setShowBackendClassifyErrorNotification: (newState: boolean) => void
-) {
+// Checks if the user has a backend Classification error logged in the database.
+// Returns: Boolean value for if a backend error was found.
+export async function checkBackendClassifyErrorStatus(): Promise<boolean> {
   // Check the database Company object for a backend Classification error status.
   const errorCheckResponse = await checkBackendClassifyError();
 
   // Check for an error getting the backend Classification error status.
   if (errorCheckResponse.queryResult.result === 'Error') {
-    // If check failed, assume no error status was logged.
+    // If check failed, assume no error status was logged and return false.
     console.error(errorCheckResponse.queryResult.message);
-    return;
+    return false;
   }
 
-  // Check if an backend Classification error staus was recorded.
-  if (errorCheckResponse.errorStatus) {
-    // Update the error state and display the frontend element to inform the user.
-    setBackendClassifyError(true);
-    setShowBackendClassifyErrorNotification(true);
-  }
+  // If the values was successfuly found, return the boolean database error value to the caller.
+  return errorCheckResponse.errorStatus;
 }
 
-// Attempt to dismiss a backend Classification error and update the related frontend UI element states.
-// Takes: State update methods for a string that indicates if the attempt to dismiss the error failed.
-//        Also takes state setters for if a backend Classification error was found and for showing the related UI element.
-// Returns: None, uses state updaters to pass result frontend.
-export async function dismissBackendClassifyErrorStatus(
-  setDismissResultMessage: (newState: string) => void,
-  setBackendClassifyError: (newState: boolean) => void,
-  setShowBackendClassifyErrorNotification: (newState: boolean) => void
-) {
+// Attempt to dismiss a backend Classification error from the database.
+// Returns: A boolean value indicating if the error was dismissed successfuly.
+export async function dismissBackendClassifyErrorStatus(): Promise<boolean> {
   // Update the database Company object to dismiss the backend Classification error status.
   const dismissErrorResponse = await dismissBackendClassifyError();
 
   // Check for an error dismissing the backend Classification error status.
   if (dismissErrorResponse.result === 'Error') {
     // If check failed, assume the error was not dismissed.
-    setDismissResultMessage('Error');
     console.error(dismissErrorResponse.message);
+    return false;
   } else {
-    // If the error status was dismissed successfuly, update the error status check states.
-    setBackendClassifyError(false);
-    setShowBackendClassifyErrorNotification(false);
-    setDismissResultMessage('Success');
+    // If the error status was dismissed successfuly, return a success boolean.
+    return true;
   }
 }
