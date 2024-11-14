fcf4c733e1d3e78c2ac85946b1406ea309e4d0d5
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-11-14 12:26:49 -0800

    Footer review & updates

--- src/components/site-elements/footer.tsx
+++ src/components/site-elements/footer.tsx
@@ -5,18 +5,37 @@ import { siteConfig } from '@/site-config/site';
 export const Footer = () => {
   return (
     <footer className="mt-auto bg-gray-900 p-6 text-white">
-      <div className="flex flex-col items-center space-y-4 md:flex-row md:justify-between md:space-y-0">
-        <p className="text-center text-sm md:text-base">
+      <div className="flex flex-col items-center space-y-2 sm:space-y-4">
+        <div className="grid w-full grid-cols-2 grid-rows-2 justify-evenly gap-y-4 mb:px-8 sm:flex sm:px-0 md:px-12 lg:px-20">
+          <Link
+            key={'/add-sbk-instructions'}
+            href={'/add-sbk-instructions'}
+            className="text-center text-lg font-semibold hover:underline">
+            Setup Instructions
+          </Link>
+          <Link
+            key={'/add-sbk-instructions'}
+            href={'/add-sbk-instructions'}
+            className="text-center text-lg font-semibold hover:underline">
+            Contact Us
+          </Link>
+          <Link
+            key={'/add-sbk-instructions'}
+            href={'/add-sbk-instructions'}
+            className="text-center hover:underline">
+            Terms of Service
+          </Link>
+          <Link
+            key={'/add-sbk-instructions'}
+            href={'/add-sbk-instructions'}
+            className="text-center hover:underline">
+            Privacy Policy
+          </Link>
+        </div>
+        <p className="text-center text-base italic">
           &copy; {new Date().getFullYear()} {siteConfig.name}. All Rights
           Reserved.
         </p>
-        <div className="flex flex-wrap justify-center space-x-4">
-          {siteConfig.footerItems.map((item) => (
-            <Link key={item.href} href={item.href} className="hover:underline">
-              {item.label}
-            </Link>
-          ))}
-        </div>
       </div>
     </footer>
   );
