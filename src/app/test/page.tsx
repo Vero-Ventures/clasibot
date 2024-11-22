7fd0bdd1f54b413322d9957e63bba3c68b7a25d7
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-11-22 13:30:59 -0800

    Remove testing file.

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DeactivationButton } from '@/components/inputs/index';
-
-export default async function Page() {
-  return (
-    <div className={`hidden pl-4 pr-2 md:mt-6 md:block`}>
-      <DeactivationButton connectionStatus={true} />
-    </div>
-  );
-}
