d15dbbb3136a8b2f94a3a754054cca99d5d21f2f
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-11-25 12:17:30 -0800

    test post-login without subscription or connection check.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,10 @@
-import { checkSubscription } from '@/actions/stripe';
+// import { checkSubscription } from '@/actions/stripe';
 
-import { checkCompanyConnection } from '@/actions/backend-actions/database-functions/index';
+// import { checkCompanyConnection } from '@/actions/backend-actions/database-functions/index';
 
 // import { SBKConfirmationModal } from '@/components/modals/index';
 
-import SubscriptionPurchase from '@/components/check-pages/subscription-purchase';
+// import SubscriptionPurchase from '@/components/check-pages/subscription-purchase';
 
 import ReviewPage from '@/components/review-page/review-page';
 
@@ -18,9 +18,9 @@ import type { CompanyInfo } from '@/types';
 
 export default async function Page() {
   // Get user subscription and check their status.
-  const subscriptionStatus = await checkSubscription();
+  // const subscriptionStatus = await checkSubscription();
   // Check if the Synthetic BookKeeper is connected to the account.
-  const companyHasSBK = await checkCompanyConnection();
+  // const companyHasSBK = await checkCompanyConnection();
 
   // Get the Company Info from the QuickBooks functions.
   const userCompanyName = await getCompanyName();
@@ -34,26 +34,14 @@ export default async function Page() {
     location: userCompanyLocation,
   };
 
-  console.log('company info: ' + companyInfo)
-  console.log('subscription: ' + subscriptionStatus)
-  console.log('connection: ' + companyHasSBK)
+  console.log('company info: ' + companyInfo);
+  // console.log('subscription: ' + subscriptionStatus)
+  // console.log('connection: ' + companyHasSBK)
 
-  if ('error' in subscriptionStatus || subscriptionStatus.valid) {
-    // If the user status is invalid or there is an error, go to the subscription purchase.
-    return <SubscriptionPurchase />;
-  } else if (
-    !companyHasSBK.connected &&
-    process.env.APP_CONFIG === 'production'
-  ) {
-    // Only perform check if in production mode.
-    console.log(`${companyHasSBK.result}: ${companyHasSBK.message}`);
-    // return <SBKConfirmationModal />;
-  } else {
-    // Otherwise, show the review page.
-    return (
-      <div className="container mx-auto px-4 py-8">
-        <ReviewPage companyInfo={companyInfo} />
-      </div>
-    );
-  }
+  // Otherwise, show the review page.
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <ReviewPage companyInfo={companyInfo} />
+    </div>
+  );
 }
