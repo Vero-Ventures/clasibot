05cc2f0c69162a2836fbeeb67ead725f6f750049
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-11-28 11:35:49 -0800

    test log changes.

--- src/app/api/qbo_invites/connect_to_company/route.ts
+++ src/app/api/qbo_invites/connect_to_company/route.ts
@@ -5,20 +5,22 @@ export async function POST(request: Request) {
     console.log('Connect To Company')
 
     // Get the Authorization header from the request.
-    const authorizationHeader = request.headers.get('Authorization');
+    const headers = request.headers;
+
+    console.log(headers)
 
     // Check for an auth header that matches the expeced value, defined by the EMAIL_ENDPOINT_AUTH env.
-    if (
-      !authorizationHeader ||
-      authorizationHeader !== process.env.EMAIL_ENDPOINT_AUTH
-    ) {
-      console.error(
-        'Error Adding Company Connection: Missing Or Invalid Authorization Header.'
-      );
-      return new Response('Missing Or Invalid Authorization Header', {
-        status: 401,
-      });
-    }
+    // if (
+    //   !authorizationHeader ||
+    //   authorizationHeader !== process.env.EMAIL_ENDPOINT_AUTH
+    // ) {
+    //   console.error(
+    //     'Error Adding Company Connection: Missing Or Invalid Authorization Header.'
+    //   );
+    //   return new Response('Missing Or Invalid Authorization Header', {
+    //     status: 401,
+    //   });
+    // }
 
     // Get request body that contains the User email name and connected Company name.
     const body = await request.json();
