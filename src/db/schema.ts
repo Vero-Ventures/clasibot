35c07add8c4a230754a687bdef3ca6ff52393f28
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-12-05 14:51:54 -0800

    database updates for accidental repeat name

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -92,8 +92,8 @@ export const ForReviewTransaction = pgTable('ForReviewTransaction', {
   acceptType: text('accept_type').notNull(),
   transactionTypeId: text('transaction_type_id').notNull(),
   payeeNameId: text('payee_name_id'),
-  topCategoryClassification: text('top_classification').notNull(),
-  topTaxCodeClassification: text('top_classification').notNull(),
+  topCategoryClassification: text('top_category_classification').notNull(),
+  topTaxCodeClassification: text('top_tax_code_classification').notNull(),
 });
 
 export const ForReviewTransactionToCategoriesRelationship = relations(
