a49a10a123e0bdda3cba880b6ec0b0b5abd209c0
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-12-05 15:00:26 -0800

    Update database with correct typing for amounts.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,6 +6,7 @@ import {
   uuid,
   text,
   integer,
+  decimal,
   boolean,
   serial,
 } from 'drizzle-orm/pg-core';
@@ -88,7 +89,7 @@ export const ForReviewTransaction = pgTable('ForReviewTransaction', {
   description: text('description').notNull(),
   origDescription: text('orig_description').notNull(),
   date: text('date').notNull(),
-  amount: integer('amount').notNull(),
+  amount: decimal('amount').notNull(),
   acceptType: text('accept_type').notNull(),
   transactionTypeId: text('transaction_type_id').notNull(),
   payeeNameId: text('payee_name_id'),
