f81f716f335b683b9ce4484fcc78b310518b0a56
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-12-10 16:35:36 -0800

    Database drizzle re-made as single snapshot.

--- drizzle/0005_robust_captain_cross.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "TaxCode" RENAME COLUMN "taxCode" TO "tax_code";--> statement-breakpoint
-ALTER TABLE "TaxCode" DROP CONSTRAINT "TaxCode_taxCode_unique";--> statement-breakpoint
-ALTER TABLE "TaxCode" ADD CONSTRAINT "TaxCode_tax_code_unique" UNIQUE("tax_code");
