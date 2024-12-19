ad15e45906dd301ed99b17409fe816157b24af0c
B. Rogers <113314466+BRogers-BCIT@users.noreply.github.com>
2024-12-19 12:08:37 -0800

    Format undo button layout.

--- src/components/review-page-components/review-table-pages-and-save.tsx
+++ src/components/review-page-components/review-table-pages-and-save.tsx
@@ -54,9 +54,9 @@ export function ReviewTablePagesAndSave({
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-between pt-2 sm:flex-row xl:px-12">
-      <div className="mb-6 flex h-12 w-full flex-row items-center px-2 mb:px-4 sm:mb-0">
-        <div className="mr-2 mt-1 h-fit min-w-[75px] py-2 text-center text-sm text-muted-foreground mb:mr-4 mb:min-w-[90px] sm:mr-0 sm:w-1/5 sm:min-w-[110px] sm:pr-6 lg:w-1/6">
+    <div className="flex flex-col items-center justify-between pt-2 md:flex-row xl:px-12">
+      <div className="mb-6 flex h-12 w-full flex-row items-center px-2 mb:px-4 md:mb-0 md:w-1/2">
+        <div className="mr-2 mt-1 h-fit min-w-[75px] py-2 text-center text-sm text-muted-foreground mb:mr-4 mb:min-w-[90px] md:mr-0 md:w-1/5 md:min-w-[110px] lg:w-1/6">
           {table.getState().pagination.pageIndex < 0
             ? 0
             : table.getState().pagination.pageIndex * pageSize}{' '}
@@ -70,7 +70,7 @@ export function ReviewTablePagesAndSave({
             </HoverCardContent>
             <HoverCardTrigger asChild>
               <Button
-                className="w-full min-w-20 max-w-36 rounded-md border-2 border-gray-300 bg-white p-2 py-2 text-base font-semibold text-black shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:border-blue-100 hover:bg-blue-300 hover:ring md:h-11 md:max-w-40 md:text-lg lg:max-w-48"
+                className="w-full min-w-20 max-w-36 rounded-md border-2 border-gray-300 bg-white p-2 py-2 text-base font-semibold text-black shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:border-blue-100 hover:bg-blue-300 hover:ring md:h-11 md:max-w-32 md:text-lg lg:max-w-48"
                 onClick={() => table.previousPage()}
                 disabled={!table.getCanPreviousPage()}>
                 Previous
@@ -82,7 +82,7 @@ export function ReviewTablePagesAndSave({
             <HoverCardContent>{'View Next 10 Transactions'}</HoverCardContent>
             <HoverCardTrigger asChild>
               <Button
-                className="w-full min-w-20 max-w-36 rounded-md border-2 border-gray-300 bg-white p-2 py-2 font-sans text-base font-semibold text-black shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:border-blue-100 hover:bg-blue-300 hover:ring md:h-11 md:max-w-40 md:text-lg lg:max-w-48"
+                className="w-full min-w-20 max-w-36 rounded-md border-2 border-gray-300 bg-white p-2 py-2 font-sans text-base font-semibold text-black shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:border-blue-100 hover:bg-blue-300 hover:ring md:h-11 md:max-w-32 md:text-lg lg:max-w-48"
                 onClick={() => table.nextPage()}
                 disabled={!table.getCanNextPage()}>
                 Next
@@ -92,11 +92,11 @@ export function ReviewTablePagesAndSave({
         </div>
       </div>
 
-      <div className="flex w-full justify-center sm:mr-4 sm:w-1/4 md:mr-8">
+      <div className="flex w-full justify-evenly md:w-1/2">
         <Button
           onClick={() => undoForReviewSave()}
-          disabled={enableUndo}
-          className="w-full min-w-20 max-w-36 rounded-md border-2 border-gray-300 bg-white p-2 py-2 font-sans text-base font-semibold text-black shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:border-blue-100 hover:bg-blue-300 hover:ring md:h-11 md:max-w-40 md:text-lg lg:max-w-48">
+          disabled={!enableUndo}
+          className="h-12 w-1/3 rounded-md border-2 border-gray-300 bg-white p-2 py-2 font-sans text-base font-semibold text-black shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:border-blue-100 hover:bg-blue-300 hover:ring md:min-w-32 md:max-w-52">
           Undo Save
         </Button>
         <Button
@@ -104,7 +104,7 @@ export function ReviewTablePagesAndSave({
           disabled={
             isSaving || table.getFilteredSelectedRowModel().rows.length === 0
           }
-          className="h-12 w-1/2 rounded bg-gradient-to-r from-blue-500 to-blue-600 px-4 py-2 text-lg font-bold text-white shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:from-blue-700 hover:to-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 sm:w-full sm:min-w-32 sm:max-w-52 xl:mr-4">
+          className="h-12 w-1/3 rounded bg-gradient-to-r from-blue-500 to-blue-600 px-4 py-2 text-lg font-bold text-white shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:from-blue-700 hover:to-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 sm:min-w-32 sm:max-w-52">
           {isSaving ? 'Saving...' : 'Save'}
         </Button>
       </div>
